3ddd8e16e442003fddc72226a1dfe16df6ba8177
Chloe Ronteix <chloe.ronteix@gmail.com>
2020-09-05 15:44:40 +0200

    fonctions scrap

--- scrap_allocine_pages.py
+++ scrap_allocine_pages.py
@@ -15,28 +15,117 @@ def get_pages(url, nb):
 pages = get_pages(url,2)
 
 df2 = pd.DataFrame(columns=('titre', 'id', 'acteurs', 'realisateur', 'date de sortie', 'genres', 'synopsis'))
-for i in pages:
 
+
+def get_films_box(pagesIndex):
     response = requests.get(i)
     print(response)
-         
     soup = bs4.BeautifulSoup(response.text, 'html.parser')
-    #print(soup)
-    em_box = soup.find_all("div", {"class":"entity-card-list"})
+    films_box = soup.find_all("div", {"class":"entity-card-list"})
+    return films_box
+
+def get_title(film):
+    title_url = film.find("a", {"class":"meta-title-link"})
+    title = title_url.text
+    return title
+
+def get_id(film):
+    title_url = film.find("a", {"class":"meta-title-link"})
+    url = title_url['href']
+    start = url.index('=')+1
+    end = url.index('.')
+    id_film = int(url[start:end])
+    return id_film
+
+def get_actors(film):
+    actors = []
+    actors_film = film.find('div', {'class':'meta-body-actor'})
+    for acteurs in actors_film:
+        acteurs = acteur.find_all(["a","span"], class_=(lambda x: x != 'light'))
+        for acteur in acteurs:
+            actors.append(acteur.text)
+    return actors
+
+def get_styles(film):
+    meta_body_info = film.find("div", {"class":"meta-body-info"})
+    genres_film = meta_body_info.find_all("span", class_=lambda x: x != 'date' and x != 'spacer')
+    genres =[]
+    for genre in genres_film:
+        genres.append(genre.text)
+    return genres
+
+def get_date(film):
+    date = film.find('span', {'class':'date'}).text
+    return date
+
+def get_real(film):
+    real = film.find_all("a", {"class":"blue-link"})
+    realisateurs = []
+    for realisateur in real:
+        realisateurs.append(realisateur.text)
+    return realisateurs
+
+def get_synopsis(film):
+    synops = film.find_all("div", {"class":"content-txt"})
+    synopsis=[]
+    synopsis_clean=[]
+    for i in synops:
+        synopsis.append(i.text)
+    for i in synopsis:
+        synopsis_clean.append((" ".join(i.split())))
+    return synopsis_clean
+
+def get_note_presse(film):
+    evaluation = film.find_all('div', {'class':'rating-holder'})
+    note_presse = 0
+    for rating in evaluation:
+        note = rating.find('span', {'class': 'stareval-note'})
+        note_presse += (float((note.text).replace(',', '.')))
+    return note_presse
+
+def get_note_spectateurs(film):
+    notes_spec = 0
+    evaluation = film.find_all('div', {'class':'rating-holder'})
+    evas = []
+    for eva in evaluation:
+        rat = eva.find_all('span')
+        evas.append(rat)
+    list_notes = []
+    for note in evas:
+        del note[0:3]
+        del note[-1]
+        list_notes.append(note)
+    for i in list_notes:
+        for note in i:
+            notes_spec += float((note.text).replace(',', '.'))
+    return notes_spec
+
+
+
+
+
+for i in pages:
+
+    # response = requests.get(i)
+    # print(response)
+         
+    # soup = bs4.BeautifulSoup(response.text, 'html.parser')
+    # #print(soup)
+    # em_box = soup.find_all("div", {"class":"entity-card-list"})
     print(len(em_box))
     print(em_box[-1].find("a", {"class":"meta-title-link"}).text)
         
         
     for film in em_box:
         # Titre
-        title_url = film.find("a", {"class":"meta-title-link"})
-        title = title_url.text
+        # title_url = film.find("a", {"class":"meta-title-link"})
+        # title = title_url.text
         #print(title)
         #Id du film
-        url = title_url['href']
-        start = url.index('=')+1
-        end = url.index('.')
-        id_film = int(url[start:end])
+        # url = title_url['href']
+        # start = url.index('=')+1
+        # end = url.index('.')
+        # id_film = int(url[start:end])
         #print(id_film)
         #Acteurs
         acteur_clean = []
