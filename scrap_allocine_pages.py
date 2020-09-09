2442a042b1deda677ffa59303c90db11dab141c7
ChloeRonteix <59954097+ChloeRonteix@users.noreply.github.com>
2020-09-09 17:26:47 +0200

    add info in get_date

--- scrap_allocine_pages.py
+++ scrap_allocine_pages.py
@@ -31,10 +31,6 @@ def add_to_postgre(film: FilmInfo): #TODO: fonction pour envoyer vers db
     pass
 
 
-
-
-
-
 def get_films_box(pages_index: int):
     url = base_url + str(pages_index)
     response = requests.get(url)
@@ -75,7 +71,7 @@ def get_styles(film):
 
 def get_date(film): #TODO: convert date
     date = film.find('span', {'class':'date'}).text
-    return date
+    return date # Voir dateparser.parse(date_string).date()
 
 def get_real(film):
     real = film.find_all("a", {"class":"blue-link"})
@@ -108,79 +104,10 @@ def get_notes(film):
 def get_id_actor(film):
     pass
 
+#TODO: script de scrap with function
 
 
-for i in pages:
-
-    # response = requests.get(i)
-    # print(response)
-         
-    # soup = bs4.BeautifulSoup(response.text, 'html.parser')
-    # #print(soup)
-    # em_box = soup.find_all("div", {"class":"entity-card-list"})
-    print(len(em_box))
-    print(em_box[-1].find("a", {"class":"meta-title-link"}).text)
-        
-        
-    for film in em_box:
-        # Titre
-        # title_url = film.find("a", {"class":"meta-title-link"})
-        # title = title_url.text
-        #print(title)
-        #Id du film
-        # url = title_url['href']
-        # start = url.index('=')+1
-        # end = url.index('.')
-        # id_film = int(url[start:end])
-        #print(id_film)
-        #Acteurs
-        acteur_clean = []
-        try:
-            actors = film.find('div', {'class':'meta-body-actor'})
-            acteur_clean.append((" ".join(actors.text.split()).replace('Avec ', '')))
-        except:
-            acteur_clean.append("non communiqué")
-            print(f"pas d'acteurs pour le film {title}")
-        #print(acteur_clean)
-        #Realisateur
-        real = film.find("a", {"class":"blue-link"}).text
-        #print(real)
-        #date
-        try:
-            date = film.find('span', {'class':'date'}).text
-        except:
-            date = "non communiquée"
-            print(f'pas de date pour le film {title}')
-        #print(date)
-        #Genres
-        meta_body_info = film.find("div", {"class":"meta-body-info"})
-        genres = meta_body_info.find_all("span")
-        del genres[0:3]
-        list_genre = []
-        for genre in genres:
-            list_genre.append(genre.text)
-        #synopsis
-        synopsis=[]
-        synopsis_clean=[]
-        try:
-            synops = film.find_all("div", {"class":"content-txt"})
-            for i in synops:
-                synopsis.append(i.text)
-            for i in synopsis:
-                synopsis_clean.append((" ".join(i.split())))
-        except:
-            synopsis_clean.append("non communiqué")
-            print(f'pas de synopsis pour le film {title}')
-        #print(synopsis_clean)
-        df2 = df2.append({'titre':title, 'id':id_film, 'acteurs':acteur_clean, 'realisateur':real, 'date de sortie':date, 'genres':list_genre, 'synopsis':synopsis_clean}, ignore_index=True)
-        
-        
-
-df2['Genres'] = [', '.join(map(str, l)) for l in df2['genres']]
-df2['Synopsis'] = [', '.join(map(str, l)) for l in df2['synopsis']]
-df2['Acteurs'] = [', '.join(map(str, l)) for l in df2['acteurs']]
-data = df2.drop(columns=['acteurs','synopsis','genres'])
 
 print("FINI")
 
-print(data)
+print(df)
