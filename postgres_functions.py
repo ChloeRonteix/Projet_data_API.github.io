a995f0c6589c23ef26ac20128883abf1eb9488a5
ChloeRonteix <59954097+ChloeRonteix@users.noreply.github.com>
2020-09-17 23:23:28 +0200

    put genres in jointure MOTHER FUCKER

--- postgres_functions.py
+++ postgres_functions.py
@@ -7,19 +7,40 @@ class PostgresFilmsRepository:
     def __init__(self):
         self.conn = psycopg2.connect(dbname="postgres", user="common", password="allocine", host="allocine.cnlsqrwefkra.eu-west-1.rds.amazonaws.com")
 
-    def add_film_to_postgres(self, film: FilmInfo):
+    def add_film_to_postgres(self, film: FilmInfo) -> int:
         c = self.conn.cursor()
-        try:
-            c.execute(ss.insert_film, (film.id, film.title, film.date, film.synopsis, film.notes[0], film.notes[1]))
+        c.execute(ss.get_film_id_by_provider_id, (film.provider_id,))
+        fetched = c.fetchone()
+        if fetched == None:
+            try:
+                c.execute(ss.insert_film, (film.provider_id, film.title, film.date, film.synopsis, film.notes[0], film.notes[1]))
+                film_id = c.fetchone()[0]
+            except Exception as e:
+                print(e)
             self.conn.commit()
-        except Exception as e:
-            print(e)
+        else:
+            film_id = fetched[0]
+        return film_id
 
 
-    def add_genre_to_postgres(self, genres:list):
+    def add_genre_to_postgres(self, genres:list, film_id:int):
         c = self.conn.cursor()
         for g in genres:
-            c.execute(ss.insert_genre, (g, g))
+            c.execute(ss.get_genre_id_by_name, (g,))
+            fetched = c.fetchone()
+            if fetched == None:
+                try:
+                    c.execute(ss.insert_genre, (g,))
+                    genre_id = c.fetchone()[0]
+                except Exception as e:
+                    print(e)
+                self.conn.commit()
+            else:
+                genre_id = fetched[0]
+            try:
+                c.execute(ss.insert_genre_for_film, (film_id, genre_id))
+            except Exception as e:
+                print(e)
             self.conn.commit()
     
     def get_last_page(self) -> int:
--- sql_script.py
+++ sql_script.py
@@ -1,8 +1,8 @@
 insert_film = '''
 INSERT INTO films (provider_id, title, date, synopsis, note_press, note_people) 
-VALUES (%s, %s, %s, %s, %s, %s);
+VALUES (%s, %s, %s, %s, %s, %s)
+RETURNING id;
 '''
-
 
 save_scraped_page = '''
 TRUNCATE scrap_progress;
@@ -11,12 +11,23 @@ INSERT INTO scrap_progress (page_id) VALUES (%s);
 get_last_scraped_page = "SELECT page_id FROM scrap_progress;"
 
 insert_genre = '''
-BEGIN;
-SELECT name FROM genres
-WHERE name = (%s);
-IF NOT found THEN
-    INSERT INTO genres (name)
-    VALUES (%s);
-END IF;
-END;
+INSERT INTO genres (name)
+VALUES (%s)
+RETURNING id;
+'''
+
+get_genre_id_by_name = '''
+SELECT id 
+FROM genres
+WHERE name = %s;
+'''
+
+insert_genre_for_film = '''
+INSERT INTO films_genres (id_film, id_genre)
+VALUES (%s, %s);
+'''
+
+get_film_id_by_provider_id = '''
+SELECT id FROM films
+WHERE provider_id = %s;
 '''
