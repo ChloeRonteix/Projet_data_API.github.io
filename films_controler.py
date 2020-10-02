1ec81015c99888f70a6623a3c4cfca9a3b1086a8
ChloeRonteix <59954097+ChloeRonteix@users.noreply.github.com>
2020-10-02 09:18:02 +0200

    update film_controller

--- films_controler.py
+++ films_controler.py
@@ -13,23 +13,9 @@ import helper
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-conn_string = "host='allocine.cnlsqrwefkra.eu-west-1.rds.amazonaws.com'" "dbname='postgres'" "user='common'" "password='allocine'"
+conn_string = "host='allocine.cnlsqrwefkra.eu-west-1.rds.amazonaws.com'" "dbname='postgres'" "user='reader'" "password='reader'"
 conn = psycopg2.connect(conn_string)
 print(conn)
-
-
-# @app.route('/api/v1/resources/books/all', methods=['GET'])
-# def api_all():
-#     conn_string = "host='allocine.cnlsqrwefkra.eu-west-1.rds.amazonaws.com'" "dbname='postgres'" "user='common'" "password='allocine'"
-#     conn = psycopg2.connect(conn_string)
-
-#     cursor = conn.cursor('cursor_unique_name', cursor_factory=psycopg2.extras.DictCursor)
-#     cursor.execute('SELECT * FROM films LIMIT 50')
-#     all_movies = cursor.fetchall()
-
-#     return jsonify(all_movies)
-
-
 
 @app.route('/api/v1/films/<int:film_id>', methods=['GET'])
 def get_film_by_id(film_id:int):
